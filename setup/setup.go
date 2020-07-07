69b52f226664ce88cb4e467c91eaeff242fddc27
letbecool <letbecool@github.com>
2020-07-07 16:09:17 +0545

    setting up to start with unit test

---
+++ setup/setup.go
@@ -0,0 +1,7 @@
+package main
+
+type setup struct{}
+
+func main() {
+
+}
---
+++ setup/setup_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*
+all test functions should begin with the "Test" keyword so that thesefunctions can be identified and run by the Golang testing package.
+The test function takes in thetesting.T parameter that will provide access to helper methods that can be used to write the tests.
+*/
+func TestSetup(t *testing.T) {
+	fmt.Println("Cool! successfully setup the test Enviroment : You see Testing information as below : ")
+}
